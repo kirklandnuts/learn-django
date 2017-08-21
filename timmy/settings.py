b2a73c81604485fb6ab441bcbe266de4a134ed37
timothydnguyen <nguyen.d.timothy@gmail.com>
2017-08-20 17:07:11 -0700

    first template

--- timmy/settings.py
+++ timmy/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from key import SECRET_KEY  
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SECRET_KEY = 'ouu4(u=uvc_6s(0kb+11f2%-!d-6xax93$^7=!d$z-46k^b5mh'
